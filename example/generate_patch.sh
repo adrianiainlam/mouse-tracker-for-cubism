abf615510468ca8150d354bb31047ec6d09fa028
Adrian Iain Lam <adrianiainlam@users.noreply.github.com>
2025-07-17 20:44:39 +0100

    Upgrade to Cubism 5 Release R4.1.

--- example/generate_patch.sh
+++ example/generate_patch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p demo_clean
-cp -p -r CubismSdkForNative-5-r.4/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_clean/
+cp -p -r CubismSdkForNative-5-r.4.1/Samples/OpenGL/Demo/proj.linux.cmake/* ./demo_clean/
 diff -pruN --exclude build ./demo_clean ./demo_dev > ./demo.patch
